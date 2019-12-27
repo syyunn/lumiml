db3ed43694bcef26eb4e8eae2675c8a4652dd2a0
Zachary <syyun@snu.ac.kr>
2019-12-27 23:18:15 +0900

    Class Abstraction  of Data prep

--- data/strexpo.py
+++ data/strexpo.py
@@ -19,11 +19,31 @@ import numpy as np
 import data.utils as utils
 from lumiml.simulator import StretchedExponentialDistribution
 
-time_series = np.arange(-2.5, 1, 0.007)
 
-gamma_eval = np.logspace(start=-2.5, stop=1, num=500, base=10, endpoint=True)
+class Data:
+    def __init__(self, start, end, num_points, base):
+        self.start = start
+        self.end = end
+        self.num_points = num_points
+        self.time_series = np.arange(start,
+                                     end,
+                                     (end - start) / num_points)
+        self.gamma_eval = np.logspace(start=start,
+                                      stop=end,
+                                      num=num_points,
+                                      base=base)
 
-utils.show2d(time_series, gamma_eval)
+    def show_gamma_eval(self):
+        utils.show2d(self.time_series, self.gamma_eval)
+
 
 if __name__ == "__main__":
+    start = -2.5
+    end = 1
+    num_points = 500
+    base = 10
+
+    data = Data(start=start, end=end, num_points=num_points, base=base)
+    data.show_gamma_eval()
+
     pass
